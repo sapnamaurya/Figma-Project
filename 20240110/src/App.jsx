97e1a5fb7939dc8b061b31c32fb668b80d8b23cd
sapna maurya <io5.sapnamaurya@gmail.com>
2024-01-13 00:11:44 +0530

    solution

--- 20240110/src/App.jsx
+++ 20240110/src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Started from "./components/Started";
+import Solution from "./components/Solution";
 function App() {
   return (
     <div>
       <Header />
       <Started />
+      <Solution />
       <BrowserRouter>
         <Routes>
           <Route />
---
+++ 20240110/src/components/Solution/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import solution from "../../assets/images/solution.png";
+import { Box, Typography } from "@mui/material";
+import "./style.css";
+
+function Solution() {
+  return (
+    <>
+      <Box className="solution-cont">
+        <img src={solution} alt="solution" />
+        <Box sx={{ textAlign: "center", marginTop: "4%" }}>
+          <Typography
+            sx={{ fontSize: "h3.fontSize", fontWeight: "bold" }}
+            className="problem"
+          >
+            Fresh Solution To Your Company's Hardest Problems
+          </Typography>
+          <Typography className="consulting" sx={{ fontSize: "h4.fontSize" }}>
+            Unconventional Consulting.
+          </Typography>
+          <Typography className="about">
+            We dont't believe in one -size-fits-all.We meet you where you
+            are,customizing or innovatve offerings to your unique needs and
+            culture-which is just one way that we do thing differently.
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+}
+export default Solution;
