da4c2f2845171353358dcaf24052df42dce1a6d7
sapna maurya <io5.sapnamaurya@gmail.com>
2024-02-04 13:04:01 +0530

    changes remove

--- 20240110/src/components/Header/index.jsx
+++ 20240110/src/components/Header/index.jsx
@@ -6,20 +6,12 @@ import SearchIcon from "@mui/icons-material/Search";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-scroll";
 function Header() {
-  // const Navigate = useNavigate();
-  // const redirectToRespectivePage = (e, path) => {
-  //   Navigate(`/${path}`);
-  // };
   return (
     <>
       <Box className="header-cont">
         <img src={logo} alt="logo" />
         <Box className="list-cont">
-          <Button
-            // onClick={(e) => redirectToRespectivePage(e, "start")}
-            variant="text"
-            className="buttons"
-          >
+          <Button variant="text" className="buttons">
             <Link
               className="topic cursor-pointer heading"
               activeClass="active"
@@ -33,11 +25,7 @@ function Header() {
             </Link>
           </Button>
 
-          <Button
-            // onClick={(e) => redirectToRespectivePage(e, "grow")}
-            variant="text"
-            className="buttons"
-          >
+          <Button variant="text" className="buttons">
             <Link
               className="topic cursor-pointer heading"
               activeClass="active"
@@ -50,11 +38,7 @@ function Header() {
               <Typography className="topic cursor-pointer">Grow</Typography>
             </Link>
           </Button>
-          <Button
-            // onClick={(e) => redirectToRespectivePage(e, "thrive")}
-            variant="text"
-            className="buttons"
-          >
+          <Button variant="text" className="buttons">
             <Link
               className="topic cursor-pointer heading"
               activeClass="active"
@@ -67,11 +51,7 @@ function Header() {
               <Typography className="topic cursor-pointer">Thrive</Typography>
             </Link>
           </Button>
-          <Button
-            // onClick={(e) => redirectToRespectivePage(e, "project")}
-            variant="text"
-            className="buttons"
-          >
+          <Button variant="text" className="buttons">
             <Link
               className="topic cursor-pointer heading"
               activeClass="active"
@@ -86,11 +66,7 @@ function Header() {
               </Typography>
             </Link>
           </Button>
-          <Button
-            // onClick={(e) => redirectToRespectivePage(e, "lets-meet")}
-            variant="text"
-            className="buttons"
-          >
+          <Button variant="text" className="buttons">
             <Link
               className="topic cursor-pointer heading"
               activeClass="active"
